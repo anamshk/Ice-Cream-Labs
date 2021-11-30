892c58408214bbe57b35a1d8a9f40e5922022852
elmi- <abdi.elmi@rocketmail.com>
2021-11-30 15:26:45 -0800

    moved seeds/schema back to 01_users.sql

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -9,3 +9,37 @@ CREATE TABLE users (
   password VARCHAR(255) NOT NULL,
   is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
+
+-- drop and recreate items table
+DROP TABLE IF EXISTS items CASCADE;
+
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  photo_url VARCHAR(255) NOT NULL,
+  price NUMERIC(3,2) NOT NULL,
+  inventory INTEGER NOT NULL
+);
+
+-- drop and recreate orders tables
+DROP TABLE IF EXISTS order_master CASCADE;
+DROP TABLE IF EXISTS order_line_items CASCADE;
+
+CREATE TABLE order_master (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  order_datetime TIMESTAMP,
+  estimated_time TEXT,
+  completion_datetime TIMESTAMP,
+  status TEXT
+);
+
+CREATE TABLE order_line_items (
+  id SERIAL PRIMARY KEY NOT NULL,
+  item_id INTEGER REFERENCES items(id) ON DELETE CASCADE,
+  order_master_id INTEGER REFERENCES order_master(id) ON DELETE CASCADE,
+  quantity INTEGER NOT NULL
+);
+
+
